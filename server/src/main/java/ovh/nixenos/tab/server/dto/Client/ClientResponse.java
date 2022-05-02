1efc9b2d61cceb163579ac268fdb6836c842c169
Wojciech Janota <wojciech@janota.ovh>
2022-05-02 21:59:25 +0200

    Accessors for DTO objects

--- server/src/main/java/ovh/nixenos/tab/server/dto/Client/ClientResponse.java
+++ server/src/main/java/ovh/nixenos/tab/server/dto/Client/ClientResponse.java
@@ -12,4 +12,55 @@ public class ClientResponse {
     private String phoneNumber;
 
     private String email;
+
+    public ClientResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
