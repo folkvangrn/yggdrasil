9402a11319d7e836eefe338797e43c2301341400
Michał Foks <michalfoks3@gmail.com>
2022-05-03 13:47:33 +0200

    Add accessors to dto classes

--- server/src/main/java/ovh/nixenos/tab/server/dto/vehicle/VehicleDtoRequest.java
+++ server/src/main/java/ovh/nixenos/tab/server/dto/vehicle/VehicleDtoRequest.java
@@ -1,20 +1,33 @@
 package ovh.nixenos.tab.server.dto.vehicle;
 
+import ovh.nixenos.tab.server.entities.VehicleType;
+
 public class VehicleDtoRequest {
     private String vin;
-    private String vehicleClass;
-    private int clientId;
+    private VehicleType vehicleClass;
+    private Long clientId;
 
     public void setVin(String vin) {
         this.vin = vin;
     }
 
-    public void setVehicleClass(String vehicleClass) {
+    public void setVehicleClass(VehicleType vehicleClass) {
         this.vehicleClass = vehicleClass;
     }
 
-    public void setClientId(int clientId) {
+    public void setClientId(Long clientId) {
         this.clientId = clientId;
     }
 
+    public String getVin() {
+        return vin;
+    }
+
+    public VehicleType getVehicleClass() {
+        return vehicleClass;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
 }
--- server/src/main/java/ovh/nixenos/tab/server/dto/vehicle/VehicleDtoResponse.java
+++ server/src/main/java/ovh/nixenos/tab/server/dto/vehicle/VehicleDtoResponse.java
@@ -3,9 +3,49 @@ package ovh.nixenos.tab.server.dto.vehicle;
 public class VehicleDtoResponse {
     private String vin;
     private String vehicleClass;
-    private int clientId;
+    private Long clientId;
     private String clientFirstName;
     private String clientLastName;
-    //^^ maybe info about client will be useful?
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public void setVehicleClass(String vehicleClass) {
+        this.vehicleClass = vehicleClass;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setClientFirstName(String clientFirstName) {
+        this.clientFirstName = clientFirstName;
+    }
+
+    public void setClientLastName(String clientLastName) {
+        this.clientLastName = clientLastName;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public String getVehicleClass() {
+        return vehicleClass;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public String getClientFirstName() {
+        return clientFirstName;
+    }
+
+    public String getClientLastName() {
+        return clientLastName;
+    }
+//^^ maybe info about client will be useful?
     //private List<int> listRequests; -> I'm not sure if we need this - maybe no?
 }
