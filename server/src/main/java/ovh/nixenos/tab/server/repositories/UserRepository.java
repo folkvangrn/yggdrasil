3dbaf93053651c1b7d3a140da48fe8d044a0e73a
Michał Foks <michalfoks3@gmail.com>
2022-05-04 22:32:13 +0200

    Change user id to Long

--- server/src/main/java/ovh/nixenos/tab/server/repositories/UserRepository.java
+++ server/src/main/java/ovh/nixenos/tab/server/repositories/UserRepository.java
@@ -5,6 +5,6 @@ import ovh.nixenos.tab.server.users.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByUsername(String username);
-    User findById(int id);
+    User findById(Long id);
 
 }
