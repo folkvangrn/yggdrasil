7aa3b6e553dc6c5adf4fe2143d09bf9907ac1b0f
Michał Foks <michalfoks3@gmail.com>
2022-05-06 18:50:10 +0200

    Add endpoint for editing vehicle and some sec exceptions

--- server/src/main/java/ovh/nixenos/tab/server/restcontrollers/VehicleController.java
+++ server/src/main/java/ovh/nixenos/tab/server/restcontrollers/VehicleController.java
@@ -1,15 +1,17 @@
 package ovh.nixenos.tab.server.restcontrollers;
 
+import org.modelmapper.MappingException;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.server.ResponseStatusException;
-import ovh.nixenos.tab.server.dto.vehicle.VehicleDtoRequest;
-import ovh.nixenos.tab.server.dto.vehicle.VehicleDtoResponse;
+import ovh.nixenos.tab.server.dto.vehicle.VehicleRequest;
+import ovh.nixenos.tab.server.dto.vehicle.VehicleResponse;
 import ovh.nixenos.tab.server.entities.Client;
 import ovh.nixenos.tab.server.entities.Vehicle;
 import ovh.nixenos.tab.server.entities.VehicleType;
+import ovh.nixenos.tab.server.exceptions.InvalidArgumentException;
 import ovh.nixenos.tab.server.services.ClientService;
 import ovh.nixenos.tab.server.services.VehicleService;
 
@@ -30,43 +32,44 @@ public class VehicleController {
     private ModelMapper modelMapper;
 
     @GetMapping
-    public List<VehicleDtoResponse> getAll(
+    public List<VehicleResponse> getAll(
             @RequestParam(value = "clientid", required = false) Long clientId) {
         List<Vehicle> vehicles;
         if(clientId == null)
             vehicles = this.vehicleService.findAll();
         else
             vehicles = this.vehicleService.findVehiclesByClientId(clientId);
-        List<VehicleDtoResponse> vehicleList = new ArrayList<>();
+        List<VehicleResponse> vehicleList = new ArrayList<>();
         for (Vehicle vehicle : vehicles) {
-            VehicleDtoResponse vehicleDtoResponse = this.modelMapper.map(vehicle, VehicleDtoResponse.class);
+            VehicleResponse vehicleDtoResponse = this.modelMapper.map(vehicle, VehicleResponse.class);
             vehicleList.add(vehicleDtoResponse);
         }
         return vehicleList;
     }
 
     @GetMapping(value = "/{vin}")
-    public VehicleDtoResponse findByVin(@PathVariable String vin) {
+    public VehicleResponse findByVin(@PathVariable String vin) {
         if(this.vehicleService.existsById(vin))
-            return this.modelMapper.map(this.vehicleService.findByVin(vin), VehicleDtoResponse.class);
+            return this.modelMapper.map(this.vehicleService.findByVin(vin), VehicleResponse.class);
         else
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, "Vehicle with vin " + vin + " does not exist!");
     }
 
     @PostMapping
-    public void addVehicle(@RequestBody VehicleDtoRequest vehicleDto) {
+    public void addVehicle(@RequestBody VehicleRequest vehicleDto) {
         try {
             Vehicle vehicle = this.modelMapper.map(vehicleDto, Vehicle.class);
             Client client = clientService.findById(vehicleDto.getClientId());
             vehicle.setClient(client);
             this.vehicleService.addVehicle(vehicle);
-        }
-//          catch ( jakisCustomException? e ){
-//              throw new ResponseStatusException(
-//              HttpStatus.BAD_REQUEST, e.getMessage());
-//          }
-        catch (Exception e) {
+        } catch ( MappingException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getCause().getCause().getCause().getMessage());
+        } catch (InvalidArgumentException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (Exception e) {
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, "Error adding new vehicle");
         }
@@ -75,6 +78,24 @@ public class VehicleController {
     @GetMapping(value = "/types")
     public VehicleType[] getVehicleTypes() {
         return VehicleType.values();
+    }
+
+    @PutMapping(value = "/{vin}")
+    public void updateVehicle(@RequestBody VehicleRequest newVehicle,
+                              @PathVariable String vin){
+        if(this.vehicleService.existsById(vin)){
+            try {
+                Vehicle vehicle = this.vehicleService.findByVin(vin);
+                vehicle.setClient(this.clientService.findById(newVehicle.getClientId()));
+                vehicle.setVehicleClass(newVehicle.getVehicleClass());
+            } catch (InvalidArgumentException e) {
+                throw new  ResponseStatusException(
+                        HttpStatus.BAD_REQUEST, e.getMessage());
+            }
+        } else {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Vehicle with vin " + vin + " does not exist!");
+        }
     }
 
 }
