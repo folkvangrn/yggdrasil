b2a48b57d1b75c100c89e3d19a0b00a8f29495ac
Michał Foks <michalfoks3@gmail.com>
2022-05-12 23:14:28 +0200

    MAJOR changes in vehicle endpoint

--- server/src/main/java/ovh/nixenos/tab/server/restcontrollers/VehicleController.java
+++ server/src/main/java/ovh/nixenos/tab/server/restcontrollers/VehicleController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/vehicle")
+@RequestMapping("/api/vehicles")
 public class VehicleController {
 
     @Autowired
